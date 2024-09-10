a49b1208c14f313d6eec040057d30c1e669901bd
Kushagra  Srivastav <76401345+Kushagra7777@users.noreply.github.com>
2024-09-10 12:18:59 +0530

    Update arch_MNIST.py

--- arch_MNIST.py
+++ arch_MNIST.py
@@ -2,18 +2,27 @@ import ao_arch as ar
 
 
 description = "Basic MNIST"
+
+neurons_x = 28  # Number of neurons in the x direction (global variable)
+neurons_y = 28
 # 1 channel, 28x28=784 neurons, each corresponding to a
 # point in MNIST (downsampled to 28x28 bitmap)
-arch_i = [28 * 28]
+arch_i = [1 for x in range(neurons_x * neurons_y)]  
 # 1 channel or dimension of output, 4 neurons, corresponding to 2^4=16 binary to code for 0-9 int, the MNIST labels
 arch_z = [4]
 # No control neurons used here
 arch_c = []
 # specifies how the neurons are connected;
-# in this case, all neurons are connected randomly to a set number of others
-connector_function = "rand_conn"
-# used 360, 180 before to good success
-connector_parameters = [392, 261, 784, 4]
+connector_function = "nearest_neighbour_conn"
+Z2I_connections = True #wether want Z to I connection or not. If not specified, by default it's False. 
+random = True #if the arch_z is not equal to size of arch_i, then pick this random function, by default it's faslse
+z_ne_conn = 4 #z_in_conn -- int of random Q-input connections for Z neurons
+
+
+
+connector_parameters = [4, 4, neurons_x, neurons_y, Z2I_connections, random, z_ne_conn] #[along axis neighbours, along diagonal neighbours, ... ]
+                       
+
 arch = ar.Arch(
     arch_i, arch_z, arch_c, connector_function, connector_parameters, description
 )
